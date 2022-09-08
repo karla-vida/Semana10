c4655574c5974797563105897b92d786d26e70ca
karlacvida <karlacvida@hotmail.com>
2022-09-08 12:35:44 -0300

    exercicio 2 e 3

--- devinknowledge/devinknowledge/src/main.jsx
+++ devinknowledge/devinknowledge/src/main.jsx
@@ -5,7 +5,5 @@ import { App } from "./App";
 import "./assets/css/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
   <App />
-  // </React.StrictMode>
 );
